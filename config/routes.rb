eff5cfb20b24c39320532f8fb0ea84d275e65d2f
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-21 03:41:08 +0000

    add routes, user controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,18 @@ Rails.application.routes.draw do
     post   :add_item
     post   :remove_item
   end
+  
+  # user authentication
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+end
 
   resources :orders, only: [:create, :show]
 
@@ -73,3 +85,4 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+
