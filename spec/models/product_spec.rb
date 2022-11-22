75374418dd3264ca6c7513aff0a29b403c403539
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-22 07:52:04 +0000

    implement passing tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -19,22 +19,22 @@ RSpec.describe Product, type: :model do
 
   it 'is not valid without a name' do
     @product.name = nil
-    expect(@product).to be_valid
+    expect(@product).to_not be_valid
   end
 
   it 'is not valid without a price' do
     @product.price = ''
-    expect(@product).to be_valid
+    expect(@product).to_not be_valid
   end
 
   it 'is not valid without a quantity' do
     @product.quantity = ''
-    expect(@product).to be_valid
+    expect(@product).to_not be_valid
   end
 
   it 'is not valid without a category' do
     @product.category = nil
-    expect(@product).to be_valid
+    expect(@product).to_not be_valid
   end
 
 end
