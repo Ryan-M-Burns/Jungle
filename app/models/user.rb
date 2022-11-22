ba91ef1c06cd36eddebcd1a10206979dce229bc4
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-22 23:50:04 +0000

    update email validation to not be case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_secure_password
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: true }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password_digest, presence: true, length: { in: 4..20 }
+  before_save { email.downcase! }
 
+  
 end
