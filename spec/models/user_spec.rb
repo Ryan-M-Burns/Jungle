a795a575ffa6cb0dec7aa173280cc4971bd682b7
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-22 09:27:48 +0000

    implement passing user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.new( 
+      last_name: "El'Jonson",
+      first_name: "Lion",
+      email: "the_rock@caliban.com",
+      password_digest: "for the lion!"
+    )
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@user).to be_valid
+  end
+
+  it 'is not valid without a last name' do
+    @user.last_name = nil
+    expect(@user).to_not be_valid
+  end
+
+  it 'is not valid without a first name' do
+    @user.first_name = nil
+    expect(@user).to_not be_valid
+  end
+
+  it 'is not valid without an email' do
+    @user.email = nil
+    expect(@user).to_not be_valid
+  end
+
+  it 'is not valid without a password' do
+    @user.password_digest = nil
+    expect(@user).to_not be_valid
+  end
+
 end
