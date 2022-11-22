6589453972a34ae31b0305be193cf694e37826c4
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-22 07:47:21 +0000

    set up test file and implement passing valid test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @category = Category.new
+    
+    @product = Product.new( 
+      name: "The Lion's Sword",
+      price: 100,
+      quantity: 1,
+      category: @category
+    )
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@product).to be_valid
+  end
+
+  it 'is not valid without a name' do
+
+  end
+
+  it 'is not valid without a price' do
+
+  end
+
+  it 'is not valid without a quantity' do
+
+  end
+
+  it 'is not valid without a category' do
+
+  end
+  
 end
