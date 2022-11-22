946e26b70557b87098e9a7b11590ef11896d2cc7
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-22 09:27:36 +0000

    update user model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 
   has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true, length: { in: 4..20 }
 
 end
