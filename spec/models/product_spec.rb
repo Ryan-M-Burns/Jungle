64654e58a8103b849f8051f185b70a942bd9e0c7
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-22 22:21:44 +0000

    implement passing tests and separate into describe sections

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Product, type: :model do
   # include multiple testing methods for future reference
   describe ".name" do
     it 'is not valid without a name' do
-      @product.name = nil
+      @product.name = ''
       expect(@product.validate).to be false
       expect(@product.valid?).to be false
       expect(@product).to_not be_valid
@@ -33,17 +33,13 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages[0]).to include ("can't be blank")
       expect(@product.errors.full_messages.any?).to be true
     end
+
+
   end
 
   describe ".price" do
     it 'is not valid when price is not a number' do
       @product.price = :NaN
-      expect(@product.valid?).to be false
-      expect(@product.errors.full_messages).to include ("Price is not a number")
-    end
-
-    it 'is not valid when price is not an integer' do
-      @product.price = 1.1
       expect(@product.valid?).to be false
       expect(@product.errors.full_messages).to include ("Price is not a number")
     end
@@ -55,27 +51,30 @@ RSpec.describe Product, type: :model do
     end
 
     it 'is not valid when price is empty' do
-      @product.quantity = ''
+      @product.price = :blank
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include ("Price is not a number")
+    end
+  end
+
+  describe ".quantity" do    
+    it 'is not valid when quantity is blank' do
+      @product.quantity = :blank
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include ("Quantity can't be blank")
+    end
+
+    it 'is not valid when quantity is not a number' do
+      @product.quantity = :NaN
       expect(@product.valid?).to be false
       expect(@product.errors.full_messages).to include ("Quantity is not a number")
     end
   end
-  it 'is not valid when quantity is blank' do
-    @product.quantity = ''
-    expect(@product.valid?).to be false
-    expect(@product.errors.full_messages).to include ("Quantity can't be blank")
+  describe ".category" do
+    it 'is not valid without a category' do
+      @product.category = nil
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include ("Category must exist")
+    end
   end
-
-  it 'is not valid when quantity is not a number' do
-    @product.quantity = 'number'
-    expect(@product.valid?).to be false
-    expect(@product.errors.full_messages).to include ("Quantity is not a number")
-  end
-
-  it 'is not valid without a category' do
-    @product.category = nil
-    expect(@product.valid?).to be false
-    expect(@product.errors.full_messages).to include ("Category must exist")
-  end
-
 end
