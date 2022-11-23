f8907eabf853f694c7fe031fdf47f34dcce65e7c
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-23 01:03:29 +0000

    update user model to check password, not password_digest, and add authenticate with credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,17 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password_digest, presence: true, length: { in: 4..20 }
+  validates :password, presence: true, length: { in: 4..20 }
   before_save { email.downcase! }
 
-  
+  def authenticate_with_credentials(email, password)
+    email = email.strip
+    email = email.downcase
+    user = this.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end 
 end
