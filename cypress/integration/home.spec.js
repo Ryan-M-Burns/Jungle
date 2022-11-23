24d841ce55f754857581bfb181e9ca02d5977350
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-23 13:28:37 -0800

    update initializer and implement working tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,16 +2,18 @@
 
 describe('example to-do app', () => {
   beforeEach(() => {
-    cy.visit('http://0.0.0.0:3000/');
+    cy.visit('/');
   });
 
   it('visits the homepage. Welcome to the Jungle! ', () => {
-    cy.visit('http://0.0.0.0:3000/');
+    cy.visit('/');
   });
 
-  it("There is products on the page", () => {
+  it("There are products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
 
-  
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 });
