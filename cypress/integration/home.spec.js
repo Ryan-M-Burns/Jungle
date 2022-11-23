87f3e58a13168a4b0bd02c3840cfd8791abc2841
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-22 22:11:44 -0800

    write first two tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,10 +1,15 @@
 /// <reference types = "cypress" />
 
 describe('example to-do app', () => {
-  beforeEach(() => { 
+  beforeEach(() => {
     cy.visit('http://0.0.0.0:3000/');
-  })
+  });
+
   it('visits the homepage. Welcome to the Jungle! ', () => {
-    cy.visit('/');
+    cy.visit('http://0.0.0.0:3000/');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
   });
 });
